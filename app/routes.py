9e2172841588c1556e53639874efd5eca9972c43
samirsepahi <samirsepahi@packer.edu>
2023-02-12 09:44:08 -0500

    Chapter 3 + Songs

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,6 +17,25 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
-@app.route('/old_chess')
-def old_chess():
-    return render_template('old_chess_site.html')
+@app.route('/songs')
+def songs():
+    user = {'username': 'Samuel'}
+    songs_list = [
+        {
+            'title': 'Let It Be', 
+            'artist': 'Beetles'
+        },
+        {
+            'title': 'Billy Jean',
+            'artist': 'Michael Jackson'
+        }
+    ]
+    return render_template('songs.html', songs_list=songs_list)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
